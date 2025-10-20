6fbd2db04438ee84ba7ea7920e95e146dd0129cc
2f1b08e <f1b08e@outlook.com>
2025-10-20 12:19:21 +0530

    add insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,5 @@
+import { SpeedInsights } from '@vercel/speed-insights/next';
+import { Analytics } from '@vercel/analytics/next';
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
@@ -28,6 +30,8 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
+        <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
