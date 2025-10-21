897b49eb2b8bf8b4f53169ecc54c4ef75f486739
2f1b08e <f1b08e@outlook.com>
2025-10-21 12:23:52 +0530

    fix up

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,10 @@
-// app/layout.tsx
-import './globals.css';
-
-export const metadata = {
-  title: 'My Next.js Docs-style Site',
-  description: 'A beautiful code & docs notebook with Next.js.',
-};
-
-export default function RootLayout({ children }) {
+ 
+export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
-      <body className="bg-[#161618] text-zinc-100">{children}</body>
+    <html>
+      <body>
+        {children}
+      </body>
     </html>
-  );
+  )
 }
