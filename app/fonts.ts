c9e3f85a1775b56f24f07dd6770ed5b130b5d7aa
Saksham <saksham.mac@icloud.com>
2025-10-24 20:05:42 +0530

    Fix paths

--- app/fonts.ts
+++ app/fonts.ts
@@ -3,17 +3,17 @@ import localFont from 'next/font/local'
 export const sfPro = localFont({
   src: [
     {
-      path: '/SF-Pro-Regular.otf',
+      path: './fonts/SF-Pro-Regular.otf',
       weight: '400',
       style: 'normal',
     },
     {
-      path: '/SF-Pro-Bold.otf',
+      path: './fonts/SF-Pro-Bold.otf',
       weight: '700',
       style: 'normal',
     },
     {
-      path: '/SF-Pro-Italic.otf',
+      path: './fonts/SF-Pro-Italic.otf',
       weight: '400',
       style: 'italic',
     },
